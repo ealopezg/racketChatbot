7b08a00364229015a296ef2a65c2c4b435bf53bd
Esteban López <esteban.lopez.g@usach.cl>
2018-04-08 16:53:58 -0300

    Añadido funcion BeginDialog

--- chatbot.scm
+++ chatbot.scm
@@ -1,28 +1,56 @@
-#lang scheme
+#!r6rs
+(import (rnrs base (6))                        ; R6RS chapter 9
+        (rnrs unicode (6))                     ; R6RS library chapter 1
+        (rnrs bytevectors (6))                 ; R6RS library chapter 2
+        (rnrs lists (6))                       ; R6RS library chapter 3
+        (rnrs sorting (6))                     ; R6RS library chapter 4
+        (rnrs control (6))                     ; R6RS library chapter 5
+        (rnrs exceptions (6))                  ; R6RS library section 7.1
+        (rnrs conditions (6))                  ; R6RS library sections 7.2 and 7.3
+        (rnrs io ports (6))                    ; R6RS library sections 8.1 and 8.2
+        (rnrs io simple (6))                   ; R6RS library sections 8.1 and 8.3
+        (rnrs files (6))                       ; R6RS library chapter 9
+        (rnrs programs (6))                    ; R6RS library chapter 10
+        (rnrs arithmetic fixnums (6))          ; R6RS library section 11.2
+        (rnrs arithmetic flonums (6))          ; R6RS library section 11.3
+        (rnrs arithmetic bitwise (6))          ; R6RS library section 11.4
+        (rnrs syntax-case (6))                 ; R6RS library chapter 12
+        (rnrs hashtables (6))                  ; R6RS library chapter 13
+        (rnrs enums)                           ; R6RS library chapter 14
+        (rnrs (6))                             ; R6RS library chapter 15
+        (rnrs eval (6))                        ; R6RS library chapter 16
+        (rnrs mutable-pairs (6))               ; R6RS library chapter 17
+        (rnrs mutable-strings (6))             ; R6RS library chapter 18
+        (rnrs r5rs (6))                        ; R6RS library chapter 19
+        (rnrs records procedural (6))          ; R6RS library section 6.3
+        (rnrs records inspection (6))          ; R6RS library section 6.4
+        (rnrs records syntactic (6))           ; R6RS library section 6.2
+        (srfi :19))
 
-;CATEGORIAS
-;HOLA CONCHETUMAREEE
-;0: Saludo
-;1: Estado de animo
 
-(define (encontrarPalabra mensaje palabra)
-  #t)
-
-(define (verCategoria mensaje)
-  (let ((lista (string-split mensaje)))))
-
-
-(define (responder mensaje)
- (encontrarFrase (verCategoria mensaje)))
+(define (saludoInicial)
+     (cond ((>= (string->number (date->string (current-date) "~H") ) 21) "Buenas noches, en que lo puedo ayudar ?")
+           ((<= (string->number (date->string (current-date) "~H") ) 6) "Buenas noches, en que lo puedo ayudar a esta hora de la madrugada?")
+           ((<= (string->number (date->string (current-date) "~H") ) 11) "Buenos dias, en que lo puedo ayudar a esta hora?")
+           ((<= (string->number (date->string (current-date) "~H") ) 20) "Buenos tardes, en que lo puedo ayudar a esta hora?")
+           )
+  )
 
 
 
-(define (leerArchivo nombre)
-  (let ((lista (file->lines nombre)))))
+(define (crearChatbot nombre personalidad)
+  '(nombre personalidad))
 
-(define (organizarFrases lista)
-  (if (eq? lista '())
-      '()
-      (if (eq? (caar (string->list lista)) #\:)
-          (append (list ( (organizarFrases (
+(define (hora)
+  (date->string (current-date) "~H:~M:~S ~d/~m/~Y")) 
+
+;FUNCION
+(define (beginDialog chatbot log seed)
+  (string-append log "\n\n" "Chat iniciado:" (hora) "\n\n" (car chatbot) ": " (saludoInicial)))
+
+
+(define cbot '("CHATBOT" 0))
+
+
+
       
